2b1b66cb6abff6028493034e75c51b3564751775
mario muuk <62722269+DrSuperMario@users.noreply.github.com>
2020-12-07 23:10:36 +0200

    added search functionality

--- models/search.py
+++ models/search.py
@@ -7,14 +7,23 @@ start = datetime(2020,11,11)
 end = datetime.now()
 
 
-def search_ticker(ticker):
-    df = pdr.DataReader(ticker, 'yahoo', start=start, end=end)
-    stock = df.to_html(render_links=True, 
-                        escape=False, 
-                        header=True,
-                        justify='left',
-                        bold_rows=True,
-                        border=0,
-                        index_names=False,
-                        classes=['table table-sm','small-text'])
+def search_ticker(ticker=str):
+    try:
+        df = pdr.DataReader(ticker.upper(), 'yahoo', start=start, end=end)
+        stock = df.to_html(render_links=True, 
+                            escape=False, 
+                            header=True,
+                            justify='left',
+                            bold_rows=True,
+                            border=0,
+                            index_names=False,
+                            classes=['table table-sm','small-text'])
+    except:
+        return """
+        </div> 
+        <div class="center-text">
+            <h1 class="lead">Ticker not found </h1> 
+        </div>    
+        """
+
     return stock
--- website.py
+++ website.py
@@ -6,37 +6,60 @@ from models.search import search_ticker
 from apirequests import news_request, crypto_request
 import models.plotly as mlpt
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = "14cxTre7gHHou"
-app.config['DEBUG'] = True
 
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
+class Web(Flask):
 
-    form = SignalForm(request.form)
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "14cxTre7gHHou"
+    app.config['DEBUG'] = True
 
-    if request.method == 'POST':
-        search = search_ticker(form.search_field.data)
-        return render_template('search.html', form=search)
+    def search():
 
-    return render_template('home.html', home=True, pressed = news_request(article_count=13), crypt_list = crypto_request())
+        form = SignalForm(request.form)
+        return search_ticker(form.search_field.data)
+        
 
-@app.route('/news', methods=['GET'])
-def news():
-    return render_template('news.html',news_list=news_request(article_count=89, stream_sentiment_data=True),news=True)
+    @app.route('/', methods=['GET', 'POST'])
+    def home():
 
-@app.route('/crypto', methods=['GET'])
-def crypto():
-    return render_template('crypto.html',crypto_list=crypto_request(index_count=89),crypto=True)
+        if request.method == 'POST':
+            return render_template('search.html', form=Web.search())
 
-@app.route('/forex', methods=['GET'])
-def forex():
-    return render_template('forex.html', forex=True)
+        return render_template('home.html', home=True, pressed = news_request(article_count=13), crypt_list = crypto_request())
 
-@app.route('/stock', methods=['GET','POST'])
-def stock():
-    return render_template('stock.html', chart_data=mlpt.plot_data_to_html(), stock=True)
+    @app.route('/news', methods=['GET','POST'])
+    def news():
+
+        if request.method == 'POST':
+            return render_template('search.html', form=Web.search())
+
+        return render_template('news.html',news_list=news_request(article_count=89, stream_sentiment_data=True),news=True)
+
+    @app.route('/crypto', methods=['GET','POST'])
+    def crypto():
+
+        if request.method == 'POST':
+            return render_template('search.html', form=Web.search())
+
+        return render_template('crypto.html',crypto_list=crypto_request(index_count=89),crypto=True)
+
+    @app.route('/forex', methods=['GET','POST'])
+    def forex():
+
+        if request.method == 'POST':
+            return render_template('search.html', form=Web.search())
+
+        return render_template('forex.html', forex=True)
+
+    @app.route('/stock', methods=['GET','POST'])
+    def stock():
+
+        if request.method == 'POST':
+            return render_template('search.html', form=Web.search())
+
+        return render_template('stock.html', chart_data=mlpt.plot_data_to_html(), stock=True)
+
 
 if __name__=="__main__":
-    app.run(port=5050)
+    Web.app.run(port=5050)
