c0a2d6b10b71a90b6310e8d25d7c3dda1f6861f3
mario muuk <62722269+DrSuperMario@users.noreply.github.com>
2021-01-08 17:37:49 +0200

    Added little date parsing

--- models/search.py
+++ models/search.py
@@ -1,49 +1,81 @@
-from datetime import datetime
-from dateutil import parser
+import requests as req
+from bs4 import BeautifulSoup as BS
+import os
 
 import pandas_datareader as pdr
 
 
 
-def search_ticker(ticker, search_start, search_field_end):
+class SearchData():
 
-    if ticker:
-        
+    def __init__(self, ticker, search_start, search_field_end):
+        self.ticker = ticker
+        self.search_start = search_start
+        self.search_field_end = search_field_end    
+    
+    
+    def search_ticker(self):
+            
+
+        if(self.ticker):
+            
+            try:
+                global df
+                global stock
+                #breakpoint()
+                df = pdr.DataReader(self.ticker.upper(), 'yahoo', 
+                                    start=self.search_start, 
+                                    end=self.search_field_end)
+                stock = df.to_html(render_links=True, 
+                                    escape=False, 
+                                    header=True,
+                                    justify='right',
+                                    bold_rows=True,
+                                    border=0,
+                                    index_names=False,
+                                    classes=['table table-sm','small-text'])
+
+                if(os.path.exists('static/temp/temp.csv')):
+                    os.remove('static/temp/temp.csv')
+
+                df.to_csv('static/temp/temp.csv')
+
+                
+            except pdr._utils.RemoteDataError:
+                return """
+                </div> 
+                <div class="center-text">
+                    <h1 class="lead">Sorry No data to Show :(  </h1> 
+                </div>    
+                """
+            except KeyError:
+                return """
+                </div> 
+                <div class="center-text">
+                    <h1 class="lead">Ticker Not found</h1> 
+                </div>    
+                """
+            
+        return stock
+
+
+    def search_info(self):
+        S = req.Session()
+        URL = f"https://www.marketwatch.com/investing/stock/{self.ticker.lower()}"
+        R = S.get(url=URL,headers={'agent_desktop':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+        'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 '
+        'Safari/537.36',}).content
         try:
-            global df
-            global stock
+            descp = BS(R, features="lxml").find_all('p',{"class","description__text"})[0].get_text()
+        except IndexError:
+            descp = """ </div> 
+                <div class="center-text">
+                    <h2 class="lead">Nothing Found</h1> 
+                </div>   """ 
 
-            df = pdr.DataReader(ticker.upper(), 'yahoo', 
-                                start=(parser.parse(search_start) if search_start else datetime(2020,11,11)), 
-                                end=(parser.parse(search_field_end) if search_field_end else datetime.now()))
-            stock = df.to_html(render_links=True, 
-                                escape=False, 
-                                header=True,
-                                justify='right',
-                                bold_rows=True,
-                                border=0,
-                                index_names=False,
-                                classes=['table table-sm','small-text'])
-            df.to_csv('static/temp/temp.csv')
+        return descp
 
             
-        except pdr._utils.RemoteDataError:
-            return """
-            </div> 
-            <div class="center-text">
-                <h1 class="lead">Error connecting server </h1> 
-            </div>    
-            """
-        except KeyError:
-            return """
-            </div> 
-            <div class="center-text">
-                <h1 class="lead">Ticker Not found</h1> 
-            </div>    
-            """       
-    return stock
-
-
         
-    
 
--- website.py
+++ website.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, session, redirect, url_for, request
+from flask import Flask, render_template, session, redirect, url_for, request, send_file
 from datetime import datetime
 from dateutil import parser
 
 from forms import SignalForm
-from models.search import search_ticker
+from models.search import SearchData
 
 from apirequests import news_request, crypto_request, forex_request, stock_request
 from models.plotly import MakePlots
@@ -15,35 +15,57 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "14cxTre7gHHou"
 app.config['DEBUG'] = True
 
-def search_data(search_start="", search_end=""):
+def search_data(search_start=None, search_end=None):
+
 
     form = SignalForm(request.form)
     search_field_start = request.form.get('search_field_start')
     search_field_end = request.form.get('search_field_end') 
-
+   
+    #stupid but fast, replace in the future
+    def check_date(date, date_type):
+        if(date == '' or date == None):
+            if(date_type == 'start'.lower()):
+                return datetime(2020,11,11)
+            elif(date_type== 'end'.lower()):
+                return datetime.now()
+        else:
+            return parser.parse(date)
+     
+         
 
     plot = MP.make_single_plot(ticker_name=str(
                                                request.form.get('search_field')).upper(),
-                                               start_t=(parser.parse(search_field_start) if isinstance(search_field_start,str) else datetime(2020,11,11)),
-                                               end_t=(parser.parse(search_field_end) if isinstance(search_field_start,str) else datetime.now())
+                                               start_t=check_date(search_field_start,'start'),
+                                               end_t=check_date(search_field_end,'end')
             )    
-        
+    
+
+    find_ticker = SearchData(form.search_field.data, 
+                        search_start=check_date(search_field_start,'start'), 
+                        search_field_end=check_date(search_field_end,'end'))
+    
     #breakpoint()
-    return plot,search_ticker(form.search_field.data, 
-                        search_start=("" if search_start is None else search_field_start), 
-                        search_field_end=("" if search_end is None else search_field_end),
-                        )
+    return plot,find_ticker.search_ticker(), find_ticker.search_info()
+
 
 @app.route('/search', methods=['GET','POST']) 
 def search():
 
     if request.method == 'POST':
+        if request.form.get('download_button'):
+            return send_file('static/temp/temp.csv',attachment_filename=f"{request.form.get('search_field')}.csv")
+
         search_field = request.form.get('search_field')
-        plot, form = search_data()
+        plot, form, info = search_data()
+      
         return render_template('search.html',
-                                date=datetime.now(), 
+                                date_f="2020-11-11",
+                                date_e=datetime.strftime(datetime.now(), '%Y-%m-%d'), 
                                 form=form,
                                 plot=plot,
+                                info=info,
+                                download_button_s = True,
                                 search_field = search_field)
 
     return render_template('search.html')
@@ -52,8 +74,8 @@ def search():
 def home():
 
     if request.method == 'POST':
-        plot, form = search_data()
-        return render_template('search.html',date=datetime.now(),form=form, plot=plot)
+        plot, form, info = search_data()
+        return render_template('search.html',date=datetime.now(),form=form, plot=plot, info=info)
 
     return render_template('home.html', home=True, pressed = news_request(article_count=14), crypt_list = crypto_request())
 
@@ -61,8 +83,8 @@ def home():
 def news():
 
     if request.method == 'POST':
-        plot, form = search_data()
-        return render_template('search.html',date=datetime.now(),form=form, plot=plot)
+        plot, form ,info = search_data()
+        return render_template('search.html',date=datetime.now(),form=form, plot=plot, info=info)
 
     return render_template('news.html',news_list=news_request(article_count=89, stream_sentiment_data=True),news=True)
 
@@ -70,8 +92,8 @@ def news():
 def crypto():
 
     if request.method == 'POST':
-        plot, form = search_data()
-        return render_template('search.html',date=datetime.now(),form=form, plot=plot)
+        plot, form, info = search_data()
+        return render_template('search.html',date=datetime.now(),form=form, plot=plot, info=info)
 
     return render_template('crypto.html',crypto_list=crypto_request(index_count=89),crypto=True)
 
@@ -79,8 +101,8 @@ def crypto():
 def forex():
 
     if request.method == 'POST':
-        plot, form = search_data()
-        return render_template('search.html',date=datetime.now(),form=form, plot=plot)
+        plot, form, info = search_data()
+        return render_template('search.html',date=datetime.now(),form=form, plot=plot, info=info)
 
     return render_template('forex.html', forex_list = forex_request(), forex=True)
 
@@ -88,8 +110,8 @@ def forex():
 def stock():
 
     if request.method == 'POST':
-        plot, form = search_data()
-        return render_template('search.html',date=datetime.now(),form=form, plot=plot)
+        plot, form, info = search_data()
+        return render_template('search.html',date=datetime.now(),form=form, plot=plot, info=info)
 
     return render_template('stock.html', chart_data=stock_request(), stock=True)
 
@@ -97,8 +119,8 @@ def stock():
 def api():
 
     if request.method == 'POST':
-        plot, form = search_data()
-        return render_template('search.html',date=datetime.now(),form=form, plot=plot)
+        plot, form, info = search_data()
+        return render_template('search.html',date=datetime.now(),form=form, plot=plot, info=info)
 
     return render_template('api.html')
 
@@ -106,8 +128,8 @@ def api():
 def about():
 
     if request.method == 'POST':
-        plot, form = search_data()
-        return render_template('search.html',date=datetime.now(),form=form, plot=plot)
+        plot, form, info = search_data()
+        return render_template('search.html',date=datetime.now(),form=form, plot=plot, info=info)
 
     return render_template('about.html')
 
